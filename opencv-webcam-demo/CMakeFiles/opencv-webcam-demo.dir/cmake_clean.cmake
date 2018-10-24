0724334d650ebabf5a83c8c468966d2e41060a2f
Lizy321 <zli17nl@gmail.com>
2018-10-24 10:49:58 +0000

    update

---
+++ opencv-webcam-demo/CMakeFiles/opencv-webcam-demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/opencv-webcam-demo.dir/opencv-webcam-demo.cpp.o"
+  "opencv-webcam-demo.pdb"
+  "opencv-webcam-demo"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/opencv-webcam-demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ video-demo/CMakeFiles/video-demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/video-demo.dir/video-demo.cpp.o"
+  "video-demo.pdb"
+  "video-demo"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/video-demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
